20b1df73fbb6106256f21fd8cca8aefec871b3f2
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-13 15:11:37 +0800

    Connect the Frontend and Backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,9 @@ const port = process.env.PORT || 8000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Hellownghgh')
 })
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({ message: 'Franz' })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
