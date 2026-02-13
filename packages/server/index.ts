1f11886a7a7202a2d830f5a06de5c13e24cfe4d5
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-13 16:28:31 +0800

    Building Backend Gemini API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { GoogleGenAI } from '@google/genai';
 import type { Request, Response } from 'express';
 
 dotenv.config();
@@ -7,11 +8,25 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hellownghgh');
+const AI = new GoogleGenAI({
+   apiKey: process.env.GEMINI_API_KEY,
 });
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Franz' });
+
+// Middleware handler for json()
+app.use(express.json());
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const { prompt } = req.body;
+   const trimmedPrompt = prompt.substring(0, 15000);
+
+   const response = await AI.models.generateContent({
+      model: 'gemini-3-flash-preview',
+      contents: trimmedPrompt,
+   });
+
+   res.json({
+      message: response.text,
+   });
 });
 
 app.listen(port, () => {
