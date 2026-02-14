0b33a7c3f9ceff2fa4b128fd582d39d00f2ec611
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-14 18:08:17 +0800

    Updated

--- packages/server/routes/chatRoutes.ts
+++ packages/server/routes/chatRoutes.ts
@@ -3,6 +3,6 @@ import { chatController } from '../controllers/chat.controller';
 
 const router = express.Router();
 
-router.post('/', sendMessage);
+router.post('/', chatController.sendMessage);
 
 export default router;
--- tests/smoke.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from 'bun:test';
-
-test('Tracking', () => {
-   expect(true).toBe(true);
-});
