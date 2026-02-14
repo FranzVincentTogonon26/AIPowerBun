e491b5bf30cc5959822a13c6e74be498d0b6c108
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-14 17:39:27 +0800

    Try Again

--- tests/smoke.test.ts
+++ tests/smoke.test.ts
@@ -1,5 +1,5 @@
-// import { expect, test } from 'bun:test';
+import { expect, test } from 'bun:test';
 
-// test('Tracking', () => {
-//    expect(true).toBe(true);
-// });
+test('Tracking', () => {
+   expect(true).toBe(true);
+});
